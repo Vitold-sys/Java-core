42bf53491fd234fa19a3b30008459e22da15e751
Vitold-sys <52232795+Vitold-sys@users.noreply.github.com>
2019-07-14 10:28:38 +0300

    Выводит вес на луне

---
+++ Lesson1_Task1.java
@@ -0,0 +1,12 @@
+package Myfirstapp;
+
+import java.util.Scanner;
+
+public class MyJavaApp {
+      public static void main(String[] args) {
+          Scanner scanner = new Scanner(System.in);
+          System.out.println("Введите ваш вес:");
+          System.out.println("Ваш вес на Луне равен: " + scanner.nextDouble()*0.16);
+              }
+          }
+
