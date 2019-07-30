aabd6f5bc252c9430677ddc6750498a53c075c3e
Vitold-sys <52232795+Vitold-sys@users.noreply.github.com>
2019-07-30 17:33:49 +0300

    Create Car.java

---
+++ Lesson4_2.0/Car.java
@@ -0,0 +1,90 @@
+package Autoparts.lesson04;
+
+import java.util.Objects;
+
+public class Car {
+    private Door door;
+    private Engine engine;
+    private Wheel wheel;
+    private Body body;
+    private Gearbox gearbox;
+
+    public Car() {
+    }
+
+    public Car(Door door, Engine engine, Wheel wheel, Body body, Gearbox gearbox) {
+        this.door = door;
+        this.engine = engine;
+        this.wheel = wheel;
+        this.body = body;
+        this.gearbox = gearbox;
+    }
+
+    public Door getDoor() {
+        return door;
+    }
+
+    public void setDoor(Door door) {
+        this.door = door;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public Wheel getWheel() {
+        return wheel;
+    }
+
+    public void setWheel(Wheel wheel) {
+        this.wheel = wheel;
+    }
+
+    public Body getBody() {
+        return body;
+    }
+
+    public void setBody(Body body) {
+        this.body = body;
+    }
+
+    public Gearbox getGearbox() {
+        return gearbox;
+    }
+
+    public void setGearbox(Gearbox gearbox) {
+        this.gearbox = gearbox;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(door, car.door) &&
+                Objects.equals(engine, car.engine) &&
+                Objects.equals(wheel, car.wheel) &&
+                Objects.equals(body, car.body) &&
+                Objects.equals(gearbox, car.gearbox);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(door, engine, wheel, body, gearbox);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "door=" + door +
+                ", engine=" + engine +
+                ", wheel=" + wheel +
+                ", body=" + body +
+                ", chassis=" + gearbox +
+                '}';
+    }
+}
