7622e80533c8a3ad21f97934d6ba166006338d48
Vitold-sys <52232795+Vitold-sys@users.noreply.github.com>
2019-08-03 22:38:34 +0300

    Update Util.java

--- Lesson4_2.0/Util.java
+++ Lesson4_2.0/Util.java
@@ -4,6 +4,4 @@ public class Util {
     public static double carPrice (Car car){
         return car.getDoor().getPrice() + car.getEngine().getPrice() + car.getWheel().getPrice() + car.getBody().getPrice() + car.getGearbox().getPrice();
     }
-    public static String carFirm = "Mercedes-benz";
-    public static String carModel = "AMG";
 }
