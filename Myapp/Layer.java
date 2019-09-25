8f606daef87cef33072ebd7d9f0716bf3a3632f5
Vitold-sys <52232795+Vitold-sys@users.noreply.github.com>
2019-09-25 15:08:32 +0300

    Create Layer.java

---
+++ Myapp/Layer.java
@@ -0,0 +1,15 @@
+package com.my.app;
+
+import java.awt.*;
+import com.my.app.Event;
+import com.my.app.Layer;
+
+public class Layer implements EventListener {
+    @Override
+    public void onEvent(Event e) {
+
+    }
+
+    public void onRender(Graphics g) {
+    }
+}
